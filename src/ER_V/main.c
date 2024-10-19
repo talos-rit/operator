f38a89412fc4cf4106f5effcc43ab86753325122
Brooke Juno Leinberger <brooke.j.leinberger@gmail.com>
2024-10-19 15:37:00 -0400

    Started implementing simulated command test demo; Parses data and manipulates endiannes

--- src/ER_V/main.c
+++ src/ER_V/main.c
@@ -1,15 +1,22 @@
+#include <stdint.h>
 #include <stdio.h>
+#include <endian.h>
+#include <string.h>
 
+#include "util/comm.h"
+#include "util/array.h"
 #include "log/log.h"
+#include "api/api.h"
 #include "sub/sub.h"
 
 #define LOG_FILE_THRESHOLD_THIS     LOG_THRESHOLD_MAX
-#define LOG_CONSOLE_THRESHOLD_THIS  LOG_THRESHOLD_DEFAULT
+#define LOG_CONSOLE_THRESHOLD_THIS  LOG_THRESHOLD_MAX
 
 int main()
 {
     LOG_init();
     LOG_start();
+    LOG_INFO("Staring demo...");
 
     SUB_init(SUB_MSG_AMQ, NULL);
     SUB_start();
@@ -18,4 +25,8 @@ int main()
 
     SUB_stop();
     SUB_destroy();
+
+    LOG_INFO("Demo Ending...");
+    LOG_stop();
+    LOG_destory();
 }
--- src/common/util/array.h
+++ src/common/util/array.h
@@ -6,6 +6,36 @@
 
 #include <stdint.h>
 
+#include "util/comm.h"
+
 #define UTIL_len(arr) (sizeof(arr)/arr[0])
 #define UTIL_modulo(a,b) (((a%b) + b) % b)
 #define STRLEN(s) sizeof(s)
+#define UTIL_BYTE_FMT "0x%02X, "
+#define UTIL_BYTE_FMT_LEN 6
+
+/**
+ * @brief Returns the length of a formated byte-string
+ * @param len Length of byte source
+ * @returns Length of C String requried to hold formated byte array on success, -1 on failure
+*/
+#define UTIL_BYTE_STR_FMT_LEN(len) (len * UTIL_BYTE_FMT_LEN + 1)
+
+/**
+ * @brief Formats the contents of a byte array into a readable C String
+ * @param dst Pointer to String destination
+ * @param src Pointer to byte source
+ * @param len Length of byte source
+ * @returns 0 on success, -1 on failure
+*/
+inline int format_byte_str(char *dst, uint8_t *src, uint16_t len)
+{
+    if (!dst || !src) return -1;
+
+    sprintf(dst, "INIT");
+    uint16_t str_iter = 0;
+    for (uint16_t iter = 0; iter < len; iter++)
+        str_iter += sprintf(&dst[str_iter], UTIL_BYTE_FMT, src[iter]);
+
+    return 0;
+}
