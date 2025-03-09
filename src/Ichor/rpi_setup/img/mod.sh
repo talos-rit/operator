3f90a3a7acf2e06643f1beb0a27caac7096fa36e
Brooke Juno Leinberger <brooke.j.leinberger@gmail.com>
2025-03-08 21:40:10 -0500

    Ichor GPIO ISR module working reliably in lab setting

--- src/Ichor/rpi_setup/img/mod.sh
+++ src/Ichor/rpi_setup/img/mod.sh
@@ -26,22 +26,24 @@ mount "$DEVICE"p1 "$MOUNT_PATH"/boot
 mount "$DEVICE"p2 "$MOUNT_PATH"/root
 
 # Setup TTL Serial console
-echo "$CMD_LINE_SERIAL" > "$MOUNT_PATH"/boot/cmdline.txt"
-echo "$CONFIG_FILE" > "$MOUNT_PATH"/boot/config.txt"
+#echo "$CMD_LINE_SERIAL" > "$MOUNT_PATH"/boot/cmdline.txt"
+#echo "$CONFIG_FILE" > "$MOUNT_PATH"/boot/config.txt"
 
 # Copy setup script and readme
-cp "$SCRIPT_DIR"/../boot/setup.sh "$MOUNT_PATH"/root/home/pi/
-cp "$SCRIPT_DIR"/README.md $MOUNT_PATH"/root/home/pi/
+#cp "$SCRIPT_DIR"/../boot/setup.sh "$MOUNT_PATH"/root/home/pi/
+#cp "$SCRIPT_DIR"/README.md $MOUNT_PATH"/root/home/pi/
 
 # Setup Operator directory
-mkdir -p /etc/talos/operator/config
-mkdir -p /etc/talos/operator/logs
-chown -R 1000:1000 /etc/talos
+#mkdir -p /etc/talos/operator/config
+#mkdir -p /etc/talos/operator/logs
+#chown -R 1000:1000 /etc/talos
 
-mkdir -p "$MOUNT_PATH"/root/home/pi/talos
-cd "$MOUNT_PATH"/root/home/pi/talos
-git clone https://github.com/talos-rit/operator.git
-cd -
+#mkdir -p "$MOUNT_PATH"/root/home/pi/talos
+#cd "$MOUNT_PATH"/root/home/pi/talos
+#git clone https://github.com/talos-rit/operator.git
+#cd -
+
+read -n1 -s -r -p $'Press any key to continue...\n' key
 
 # Clean up
 umount "$MOUNT_PATH"/boot
