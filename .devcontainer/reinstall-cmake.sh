f3fd5d272d965b280ec8bf1a5fe236172714069a
cwo3990 <cwo3990@rit.edu>
2025-11-11 16:16:07 -0500

    moved commands for installing dev tools to a separate script, fixed an error in those commands, and updated Dockerfile to run the script for installing the dev tools

--- .devcontainer/reinstall-cmake.sh
+++ .devcontainer/reinstall-cmake.sh
@@ -57,11 +57,3 @@ sh "${TMP_DIR}/${CMAKE_BINARY_NAME}" --prefix=/opt/cmake --skip-license
 
 ln -s /opt/cmake/bin/cmake /usr/local/bin/cmake
 ln -s /opt/cmake/bin/ctest /usr/local/bin/ctest
-
-PACKAGES_TO_INSTALL="bear clang-format cpputest"
-if [ "$architecture" == "amd64" ]; then
-    wget https://github.com/bkryza/clang-uml/releases/download/0.6.2/clang-uml_0.6.2-1_amd64.deb
-    PACKAGES_TO_INSTALL+="./clang-uml_0.6.2-1_amd64.deb clang-uml"
-fi
-apt-get update && export DEBIAN_FRONTEND=noninteractive
-apt-get -y install --no-install-recommends ${PACKAGES_TO_INSTALL}
