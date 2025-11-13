9f3cf4e967d6cb3c2365fc44f04e1ddbf0321e08
Ryan Yocum <rtyocum1@gmail.com>
2025-11-13 17:46:05 -0500

    added limits

--- src/Ichor/arm/ichor_arm.cpp
+++ src/Ichor/arm/ichor_arm.cpp
@@ -48,6 +48,11 @@ Ichor::Ichor(const char *isr_dev, const char *i2c_dev, uint8_t dac0_addr,
   dac[0] = new PCA9685PW(i2c_fd, dac0_addr);
   // dac[1] = new PCA9685PW(i2c_fd, dac1_addr);
   mcp_gpio = new MCP23017("/dev/i2c-1", 0x21);
+  for (uint8_t pin = 0; pin < 8; pin++) {
+    mcp_gpio->setPinMode(pin, MCP23017::Port::A, false);  // Set as input
+    mcp_gpio->setInterrupt(pin, MCP23017::Port::A,
+                           MCP23017::InterruptMode::RISING);
+  }
   // TODO: Add adc
 
   for (uint8_t idx = 0; idx < ICHOR_AXIS_COUNT; idx++) axis[idx] = NULL;
