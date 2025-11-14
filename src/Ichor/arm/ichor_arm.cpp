69c934e965786cb609e3c8ff5a95d9dd326f93d0
Ryan Yocum <rtyocum1@gmail.com>
2025-11-14 14:11:22 -0500

    limit switches

--- src/Ichor/arm/ichor_arm.cpp
+++ src/Ichor/arm/ichor_arm.cpp
@@ -87,15 +87,7 @@ void Ichor::poll() {
   }
 
   std::span<const MCP23017::InterruptPin> status =
-      mcp_gpio->getInterruptStatuses();
-
-
-  for (const auto &pin : status) {
-    if (pin.port == MCP23017::Port::A) {
-      LOG_WARN("GPIO Interrupt on MCP23017 Port A Pin %d", pin.pin);
-      // Handle specific pin interrupts here
-    }
-  }
+      mcp_gpio->getInterruptStatuses(MCP23017::Port::A);
 
   dac[0]->FlushQueues();  // Flush pending DAC writes
   usleep(25e3);  // 25 ms delay (defacto delay in Talos Operator so far)
