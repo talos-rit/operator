d4d94c8281dc321cda9acd83ea54ac1452ded905
Ryan Yocum <rtyocum1@gmail.com>
2025-11-14 19:19:40 -0500

    new_pca_driver

--- src/Ichor/arm/ichor_arm.h
+++ src/Ichor/arm/ichor_arm.h
@@ -1,30 +1,22 @@
 #pragma once
 
 #include "arm/arm.hpp"
-#include "dac/PCA9685PW.h"
-#include "driver/driver.h"
-#include "gpio/isr.h"
 #include "mcp/MCP23017.hpp"
+#include "motorhat/MotorHAT.hpp"
+#include "pwm/PCA9685.hpp"
 
 #define ICHOR_AXIS_COUNT 8
 
 class Ichor : public Arm {
+  static constexpr uint8_t ICHOR_HAT_COUNT = 2;
+
  public:
-  Ichor(const char *isr_dev, const char *i2c_dev, uint8_t dac0_addr,
-        uint8_t dac1_addr, uint8_t adc_addr);
+  Ichor(const char *i2c_dev, uint8_t dac0_addr, uint8_t dac1_addr);
   ~Ichor();
-
-  int RegisterMotor(uint8_t motor_index, uint8_t dac_index, uint8_t dac_in1,
-                    uint8_t dac_in2, uint8_t dac_speed, uint8_t enc_a,
-                    uint8_t enc_b, uint8_t adc_channel);
-
-  int Init();
 
  private:
   int i2c_fd;
-  Driver *axis[ICHOR_AXIS_COUNT];
-  PCA9685PW *dac[2];
-  IchorISR *isr;
+  MotorHAT *motor_controllers[ICHOR_HAT_COUNT];
   MCP23017 *mcp_gpio;
 
   char polar_pan_cont;
