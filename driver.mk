766ed04898a3a963676d6a04a91b9a0541becac4
Ryan Yocum <rtyocum1@gmail.com>
2026-01-21 12:30:45 -0500

    updated driver mk

--- driver.mk
+++ driver.mk
@@ -6,23 +6,23 @@ ifndef DRIVER_DIR
 DRIVER_DIR = Ichor/driver #set dir, if not set already (relative to SRC_DIR)
 endif
 
-FLAGS += -I$(SRC_DIR)/$(DRIVER_DIR)/src
+FLAGS             += -I$(SRC_DIR)/$(DRIVER_DIR)/src
 
-DRIVER		:=
+DRIVER            :=
 
-DRIVER_CPP	:= serial/serial.cpp
-DRIVER_CPP	+= serial/i2c_dev.cpp
-DRIVER_CPP	+= dac/PCA9685PW.cpp
-DRIVER_CPP	+= gpio/isr.cpp
-DRIVER_CPP 	+= enc/encoder.cpp
-DRIVER_CPP 	+= motor/motor.cpp
-DRIVER_CPP 	+= driver/driver.cpp
+DRIVER_CPP        := serial/serial.cpp
+DRIVER_CPP        += serial/i2c_dev.cpp
+DRIVER_CPP        += dac/PCA9685PW.cpp
+DRIVER_CPP        += gpio/isr.cpp
+DRIVER_CPP        += enc/encoder.cpp
+DRIVER_CPP        += motor/motor.cpp
+DRIVER_CPP        += driver/driver.cpp
 
-DRIVER_UTEST:=
+DRIVER_UTEST      :=
 
-ODIR := $(OBJ_DIR)/$(DRIVER_DIR)
-DRIVER_OBJS := $(DRIVER:%.c=$(ODIR)/%.o)
-DRIVER_OBJS += $(DRIVER_CPP:%.cpp=$(ODIR)/%.o)
+ODIR              := $(OBJ_DIR)/$(DRIVER_DIR)
+DRIVER_OBJS       := $(DRIVER:%.c=$(ODIR)/%.o)
+DRIVER_OBJS       += $(DRIVER_CPP:%.cpp=$(ODIR)/%.o)
 DRIVER_UTEST_OBJS := $(DRIVER_UTEST:%.cpp=$(ODIR)/%.o)
 
 # C
