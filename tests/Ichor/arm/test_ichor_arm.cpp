d351e24454c41fa810fa0ca9bdbb968f8ace60b9
Hiro <hiroto125takeuchi@gmail.com>
2026-01-23 00:17:28 +0000

    fix: test source code to use the new file extensions

--- tests/Ichor/arm/test_ichor_arm.cpp
+++ tests/Ichor/arm/test_ichor_arm.cpp
@@ -1,29 +1,25 @@
 #include "CppUTest/TestHarness.h"
 #include "CppUTestExt/MockSupport.h"
+#include "arm/ichor_arm.hpp"
 
-#include "arm/ichor_arm.h"
+TEST_GROUP(IchorArmTest) {
+  Ichor* ichorArm;
 
-TEST_GROUP(IchorArmTest)
-{
-    Ichor* ichorArm;
-    
-    void setup()
-    {
-        // TODO: Provide appropriate constructor arguments
-        // const char *isr_dev, const char *i2c_dev, uint8_t dac0_addr, uint8_t dac1_addr, uint8_t adc_addr
-        ichorArm = new Ichor("/dev/i2c0", 0x10, 0x11);
-    }
-    
-    void teardown()
-    {
-        delete ichorArm;
-        mock().clear();
-    }
+  void setup() {
+    // TODO: Provide appropriate constructor arguments
+    // const char *isr_dev, const char *i2c_dev, uint8_t dac0_addr, uint8_t
+    // dac1_addr, uint8_t adc_addr
+    ichorArm = new Ichor("/dev/i2c0", 0x10, 0x11);
+  }
+
+  void teardown() {
+    delete ichorArm;
+    mock().clear();
+  }
 };
 
 // TODO: Add more tests for Ichor arm functionality
-IGNORE_TEST(IchorArmTest, Initialization)
-{
-    // TODO: Mock the open system call to simulate device opening
-    CHECK_TRUE(ichorArm != NULL);
+IGNORE_TEST(IchorArmTest, Initialization) {
+  // TODO: Mock the open system call to simulate device opening
+  CHECK_TRUE(ichorArm != NULL);
 }
--- tests/common/all_tests.cpp
+++ tests/common/all_tests.cpp
@@ -1,4 +1,4 @@
 #include <CppUTest/CommandLineTestRunner.h>
 #include <CppUTest/TestHarness.h>
 
-int main(int argc, char **argv) { RUN_ALL_TESTS(argc, argv); }
+int main(int argc, char** argv) { RUN_ALL_TESTS(argc, argv); }
